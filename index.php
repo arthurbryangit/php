bc0536eb1ac6d3516915dc8a22af39149e5bc12d
Arthur Bryan <arthurbryan.ab@gmail.com>
2018-07-26 11:15:31 -0300

    mudei o primeiro echo

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 
-<<<<<<< HEAD
   echo "Olá Mundo!456"; 
-  echo "Olá Udemy!";
+  echo "Olá Udemy e Hcode!";
 
-=======
-  echo "Olá Mundo 123!"; 
-  echo "Olá Hcode"
->>>>>>> abe085b01e3c9fb6beb59df2d6baf12e14955a45
 
 ?>
